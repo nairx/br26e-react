9407102f6b249ed5993f939196951b66bbd0db89
nairx <nairxprv@gmail.com>
2026-05-26 16:09:39 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,131 @@
-import React from "react";
-function Header() {
-  return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
-}
-function Home() {
-  return (
-    <div>
-      <p>This is the home component</p>
-    </div>
-  );
-}
-function Footer() {
-  return (
-    <div>
-      <h3>&copy; 2026. All rights reserved.</h3>
-    </div>
-  );
-}
+
+//passing argument to a function
+import React from 'react'
 export default function App() {
+  const handleClick = (name) => {
+    alert(name)
+  }
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <button onClick={()=>handleClick("John")}>Click</button>
     </div>
-  );
+  )
 }
 
+//Handling events
+// import React from 'react'
+// export default function App() {
+//   const handleClick = () => {
+//     alert("Hello")
+//   }
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Click</button>
+//     </div>
+//   )
+// }
+
+
+//external styling
+// import React from "react";
+// import "./App.css"
+// export default function App() {
+//   return (
+//     <div>
+//       <h1 className="class">Heading</h1>
+//     </div>
+//   );
+// }
+
+//inline styling
+// import React from "react";
+// export default function App() {
+//   return (
+//     <div>
+//       <h1 style={{ backgroundColor: "yellow" }}>Heading</h1>
+//     </div>
+//   );
+// }
+
+//default props
+// import React from "react";
+// function Sum({a=0,b=0}) {
+//   return <div>{a+b}</div>;
+// }
+// export default function App() {
+//   return (
+//     <div>
+//       <Sum a={10} />
+//     </div>
+//   );
+// }
+
+//destructuring props
+// import React from "react";
+// function Sum({a,b}) {
+//   return <div>{a+b}</div>;
+// }
+// export default function App() {
+//   return (
+//     <div>
+//       <Sum a={10} b={4} />
+//     </div>
+//   );
+// }
+
+//props
+// import React from "react";
+// function Sum(props) {
+//   return <div>{props.a + props.b}</div>;
+// }
+// export default function App() {
+//   return (
+//     <div>
+//       <Sum a={10} b={4} />
+//     </div>
+//   );
+// }
+
+//Expressions
+// import React from "react";
+// export default function App() {
+//   let name = "John";
+//   let a = 10;
+//   let b = 20;
+//   return (
+//     <div>
+//       Hello {name}
+//       {a + b}
+//     </div>
+//   );
+// }
+
+//Example of class component
+// import React, { Component } from 'react'
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>App</div>
+//     )
+//   }
+// }
+
+//import/export components
+// import React from "react";
+// import Header from "./Header";
+// import Home from "./Home";
+// import Footer from "./Footer";
+// export default function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Home />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+//one top element
 // import React from 'react'
 // export default function App() {
 //   return (
---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <h3>&copy; 2026. All rights reserved.</h3>
+    </div>
+  );
+}
