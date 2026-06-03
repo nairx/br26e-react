6cba2b05a59b0b15de86a24b4b561659aceb80b1
nairx <nairxprv@gmail.com>
2026-06-03 15:13:32 +0530

    updated

--- src/EditUser.jsx
+++ src/EditUser.jsx
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function EditUser() {
   const [user, setUser] = useState({});
-  const {id} = useParams();
+  const { id } = useParams();
+  const Navigate = useNavigate();
   useEffect(() => {
     fetch(`http://localhost:5000/users/${id}`)
       .then((res) => res.json())
       .then((data) => setUser(data))
       .catch((err) => console.log(err));
   }, []);
+  const handleSave = async () => {
+    await axios.patch(`http://localhost:5000/users/${id}`, user);
+    Navigate("/");
+  };
   return (
     <div>
       EditUser
       <p>
-        <input type="text" value={user.name} />
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          value={user.name}
+        />
       </p>
       <p>
-        <input type="text" value={user.email} />
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          value={user.email}
+        />
       </p>
-      <button>Save</button>
+      <button onClick={handleSave}>Save</button>
     </div>
   );
 }
